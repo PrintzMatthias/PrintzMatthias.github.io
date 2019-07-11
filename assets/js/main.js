2ea1390f47a997eea19f971ee2de1bcbe72218cd
Matthias Printz <m-printz@kellogg.northwestern.edu>
2019-07-11 17:24:16 +0200

    Bug fixes und CV updates
    Google Maps raus, weil es nicht mehr funktioniert hat
    Amazon experience rein

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,10 +35,11 @@
 		/* GOOGLE MAPS
 		/*----------------------------------*/
 
+		/*
 		if( $('.map-canvas').length > 0) {
 			
 			var geocoder = new google.maps.Geocoder();
-			var address = 'Google Amsterdam, Netherlands';
+			var address = 'Google Cologne, Germany';
 			var contentString = '<div class="map-detail"><strong>Our Office:</strong><p>' + address + '</p></div>';
 			
 			geocoder.geocode({'address': address }, function(results, status) {
@@ -55,7 +56,28 @@
 				}else { alert('Google Maps had some trouble finding the address. Status: ' + status); }
 			});
 		}
-
+		*/
+		if( $('.map-canvas').length > 0) {
+     
+   		var geocoder = new google.maps.Geocoder();
+    	var address = 'Google New York, 76 Ninth Ave, New York, NY, USA'; // change it with your address
+    	var contentString = '<div class="map-detail"><strong>Our Office:</strong><p>' + address + '</p></div>';
+     
+   		geocoder.geocode({'address': address }, function(results, status) {
+        	if(status == google.maps.GeocoderStatus.OK) { 
+            	var latitude = results[0].geometry.location.lat();
+            	var longitude = results[0].geometry.location.lng();
+ 
+            	jQuery('.map-canvas').gmap().bind('init', function(ev, map) {
+                	jQuery('.map-canvas').gmap('addMarker', {'position': latitude+','+longitude, 'bounds': true}).click(function() {
+                    	jQuery('.map-canvas').gmap('openInfoWindow', {'content': contentString}, this);
+                	});
+                	jQuery('.map-canvas').gmap('option', 'zoom', 8);
+            	});
+        		}else { alert('Google Maps had some trouble finding the address. Status: ' + status); }
+    		});
+     
+		}
 
 		/*-----------------------------------/
 		/* AJAX CONTACT FORM
